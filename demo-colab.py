7feb1adbc1643b388d2969d4f15a5ce63f4f0d7e
PasLoin <25326871+PasLoin@users.noreply.github.com>
2024-08-02 08:31:10 +0200

    Create demo-colab.py

---
+++ demo-colab.py
@@ -0,0 +1,124 @@
+import argparse
+import real.plot_path as pl
+import networkx as nx
+import osmnx as ox
+import real.graph_utils as gu
+import real.graph_algo as ga
+import real.convert_utils as cu
+import real.stats as st
+import matplotlib.pyplot as plt
+
+def parse_argument():
+    if "google.colab" in sys.modules:
+        # Set default values for city and country when running in Colab
+        city = "Hampstead"
+        country = "Canada"
+        return city, country
+    else:
+        # Use argparse for command-line arguments
+        ps = argparse.ArgumentParser(description="Big demo.")
+        ps.add_argument("--city", type=str, help="Specify city to search.")
+        ps.add_argument("--country", type=str, help="Specify country to search.")
+        args = ps.parse_args()
+        if args.city is None:
+            city = "Hampstead"
+            country = "Canada"
+            return city, country
+        return args.city, args.country
+
+def main():
+    city, country = parse_argument()
+    if (city is None or country is None):
+        print("Please specify --city and --country")
+        print("Example: --city Kremlin-Bicetre --country France")
+
+    print("Specified city: ", city)
+    print("Specified country: ", country)
+
+    print("Getting map data from Osmnx")
+    # Get the graph data from osmnx
+    G = ox.graph_from_place(city + ', ' + country, network_type='drive')
+
+    # Convert the graph to undirected
+    G = ox.utils_graph.get_undirected(G)
+
+    # Steps to solve the Chinese postman problem:
+    # Step 1: Calculate list of nodes with odd degree
+    print("Step 1: Calculate list of nodes with odd degree")
+    nodes_odd_degree = gu.get_nodes_odd_degree(G)
+
+    # Step 2.1: Compute all possible pairs of odd degree nodes.
+    print("Step 2.1: Compute all possible pairs of odd degree nodes.")
+    odd_node_pairs = gu.compute_pairs_of_odd_degree_nodes(nodes_odd_degree)
+
+    # Step 2.2: Compute the shortest path between each node pair calculated in 1.
+    print("Step 2.2: Compute the shortest path between each node pair calculated in 1.")
+    odd_node_pairs_shortest_paths = ga.get_shortest_paths_distances(G, odd_node_pairs, 'distance')
+
+    # Step 2.3: Generate the complete graph
+    print("Step 2.3: Generate the complete graph")
+    g_odd_complete = ga.create_complete_graph(odd_node_pairs_shortest_paths, flip_weights=True)
+
+    node_positions = gu.get_node_position(G)
+
+    # Plot the complete graph of odd degree nodes
+    print("Plotting the complete graph of odd degree nodes")
+    fig, ax = plt.subplots()
+    pl.plot_complete_graph_odd_degree(g_odd_complete, node_positions, ax=ax)
+    plt.show()
+
+    # Step 2.4: Compute Minimum Weight Matching
+    print("Step 2.4: Compute Minimum Weight Matching")
+    odd_matching_dupes = nx.algorithms.max_weight_matching(g_odd_complete, True)
+    odd_matching = gu.remove_dupes_from_matching(odd_matching_dupes)
+
+    # Create augmented graph
+    g_aug = ga.add_augmenting_path_to_graph(G, odd_matching)
+
+    # Step 3.0: Compute Eulerian Circuit
+    print("Step 3.0: Compute Eulerian Circuit")
+    s = g_aug.edges()
+    source_s = gu.get_first_element_from_multi_edge_graphe(s)
+
+    naive_eulerian = False
+    if naive_eulerian:
+        naive_euler_circuit = list(nx.eulerian_circuit(g_aug, source=source_s))
+        euler_circuit = naive_euler_circuit
+    else:
+        euler_circuit = ga.create_eulerian_circuit(g_aug, G, source_s)
+
+    st.stats(G, euler_circuit)
+
+    # Plot the minimum matchings
+    g_odd_complete_min_edges = gu.get_nodes_odd_complete_min_edges(odd_matching)
+    print("Plotting minimum weight matching complete graph")
+    fig, ax = plt.subplots()
+    pl.plot_min_weight_matching_complete(g_odd_complete, g_odd_complete_min_edges, node_positions, ax=ax)
+    plt.show()
+
+    print("Plotting minimum weight matching original graph")
+    fig, ax = plt.subplots()
+    pl.plot_min_weight_matching_original(G, g_odd_complete_min_edges, node_positions, ax=ax)
+    plt.show()
+
+    # Convert the Euler circuit to a route
+    route = cu.euler_circuit_to_route(euler_circuit)
+    with open("output.txt", "w") as f:
+        for e in euler_circuit:
+            if e[2][0].get('name') is None:
+                continue
+            for s in (e[2][0].get('name')):
+                f.write(s)
+            f.write('\n')
+
+    long, lat = cu.route_to_long_lat(G, route)
+    origin_point, dest_point = cu.long_lat_to_points(long, lat)
+
+    print("Plotting the route")
+    # Plot the route
+    fig, ax = plt.subplots()
+    pl.plot_path(lat, long, origin_point, dest_point, ax=ax)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
